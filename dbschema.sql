56de33b20517a17959d25640df32fdbee72666e6
LolHens <pierrekisters@gmail.com>
2018-02-05 11:00:27 +0100

    added dbschema

---
+++ dbschema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE bullets
+(
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  x           INT                                       NOT NULL,
+  y           INT                                       NOT NULL,
+  r           INT                                       NOT NULL,
+  playername  VARCHAR(255)                              NOT NULL,
+  token       VARCHAR(255)                              NOT NULL,
+  lastupdated TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
+  created     TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL
+);
+
+CREATE TABLE leaderboard
+(
+  playername VARCHAR(255) NOT NULL,
+  token      VARCHAR(255) NOT NULL,
+  score      INT          NOT NULL,
+  PRIMARY KEY (playername, token)
+);
+
+CREATE TABLE players
+(
+  playername  VARCHAR(255)                        NOT NULL PRIMARY KEY,
+  token       VARCHAR(255)                        NOT NULL,
+  x           INT                                 NOT NULL,
+  y           INT                                 NOT NULL,
+  r           INT                                 NOT NULL,
+  lastupdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  score       INT DEFAULT '0'                     NOT NULL,
+  dead        CHAR DEFAULT '0'                    NOT NULL
+);
